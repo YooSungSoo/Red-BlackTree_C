2342c92b63bfa6b75f3b05036e2d0b2b38c92300
SungSoo Yoo <elcane2@ajou.ac.kr>
2025-08-18 15:12:51 +0900

    make retree_to_array()

--- rbtree_lab/src/rbtree.c
+++ rbtree_lab/src/rbtree.c
@@ -23,27 +23,51 @@ static void postorder_free(rbtree *t,node_t *x){
   free(x);
 }
 
-node_t *rbtree_insert(rbtree *t, const key_t key) {
-  // TODO: implement insert
-  return t->root;
-}
-
 node_t *rbtree_find(const rbtree *t,const key_t k){
   node_t *cur = t -> root;
   while(cur != t -> nil){
     if(k == cur -> key) return cur;
-    cur = (k < cur -> key) ? cur -> left : cur -> right;
+    else if(k > cur->key) cur = cur -> right;
+    else if(k < cur->key) cur = cur -> left;
   }
   return NULL;
 }
 
 node_t *rbtree_min(const rbtree *t) {
-  // TODO: implement find
-  return t->root;
+  node_t *cur = t->root;
+  if(cur == t->nil) return NULL;
+  while(cur->left != t->nil){  
+    cur = cur->left; 
+  }
+  return cur;
 }
 
 node_t *rbtree_max(const rbtree *t) {
-  // TODO: implement find
+  node_t *cur = t->root;
+  if(cur == t->nil) return NULL;
+  while(cur->right != t->nil){  
+    cur = cur->right; 
+  }
+  return cur;
+}
+
+int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
+  size_t i = 0;
+  inorder(t, t->root, arr, &i, n);
+  return 0;
+}
+
+// size_t 변수를 쓰는 이유 - arr의 정보 저장 인덱싱을 위해
+static void inorder(const rbtree *t, const node_t *node, key_t *arr, size_t *i, const size_t n) {
+  if (node == t->nil || *i >= n) return;
+  inorder(t, node->left, arr, i, n);
+  if (*i < n) arr[(*i)++] = node->key;
+  inorder(t, node->right, arr, i, n);
+}
+
+
+node_t *rbtree_insert(rbtree *t, const key_t key) {
+  // TODO: implement insert
   return t->root;
 }
 
@@ -52,7 +76,3 @@ int rbtree_erase(rbtree *t, node_t *p) {
   return 0;
 }
 
-int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
-  // TODO: implement to_array
-  return 0;
-}
